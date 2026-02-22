7d88d6974f20df91674288170a62c165bfe81000
Bondarev Evgeni Yurievich <bond.evg03@mail.ru>
2026-02-22 19:38:44 +0300

    Изменение основного домена на "studiob2b.ru" и удаление метода получения основного домена

--- StudioB2B.Infrastructure/MultiTenancy/UrlExtensions.cs
+++ StudioB2B.Infrastructure/MultiTenancy/UrlExtensions.cs
@@ -9,12 +9,4 @@ public static class UrlExtensions
 
         return $"https://{subdomain}.{masterDomain}{path}";
     }
-
-    public static string GetMainDomainUrl(this string masterDomain, string path = "/")
-    {
-        masterDomain = masterDomain.TrimStart("https://".ToCharArray())
-            .TrimStart("http://".ToCharArray());
-
-        return $"https://{masterDomain}{path}";
-    }
 }
