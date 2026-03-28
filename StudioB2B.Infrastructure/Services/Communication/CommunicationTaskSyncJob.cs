4425a717a7fe7e3200522eb3b70e86d115d440b2
Bondarev Evgeni Yurievich <bond.evg03@mail.ru>
2026-03-28 23:31:46 +0300

    импорты

--- StudioB2B.Infrastructure/Services/Communication/CommunicationTaskSyncJob.cs
+++ StudioB2B.Infrastructure/Services/Communication/CommunicationTaskSyncJob.cs
@@ -5,6 +5,7 @@ using StudioB2B.Domain.Constants;
 using StudioB2B.Domain.Entities;
 using StudioB2B.Infrastructure.Interfaces;
 using StudioB2B.Infrastructure.Persistence.Tenant;
+using StudioB2B.Shared;
 using StudioB2B.Shared.DTOs;
 
 
