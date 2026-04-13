93ce50712b7f47b27a1adfa8fb49d9efb6ff2879
javaro3 <korol2000041@gmail.com>
2026-04-13 09:50:13 +0300

    update reserved subdomains for multi-tenancy configuration

--- StudioB2B.Domain/Options/MultiTenancyOptions.cs
+++ StudioB2B.Domain/Options/MultiTenancyOptions.cs
@@ -6,7 +6,7 @@ public class MultiTenancyOptions
 
     public string MasterDomain { get; set; } = "studiob2b.ru";
 
-    public string[] ReservedSubdomains { get; set; } = ["app", "www", "api", "admin", "master", "minio", "seq"];
+    public string[] ReservedSubdomains { get; set; } = ["app", "www", "api", "admin", "master", "minio", "seq", "chat", "matrix", "dimension"];
 
     public string TenantDbConnectionTemplate { get; set; } = "Server=localhost;Database={0};User=root;Password=root;Allow User Variables=true;AllowPublicKeyRetrieval=True;";
 }
